5568a78e45d8efe837141318d302d2284dafca80
Chinosu <46995931+Chinosu@users.noreply.github.com>
2024-02-08 21:27:20 +1100

    Refactor top bar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,17 +2,18 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {GravitySliderComponent} from "./gravity-slider/gravity-slider.component";
 import {X86LoopComponent} from "./x86-loop/x86-loop.component";
-import {TopMenuComponent} from "./top-menu/top-menu.component";
 import {CubeComponent} from "./cube/cube.component";
 import {PhoneComponent} from "./phone/phone.component";
 import {LeftRightComponent} from "./left-right/left-right.component";
+import {NgOptimizedImage} from "@angular/common";
+import {ClusterContainerComponent} from "./cluster-container/cluster-container.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [
     RouterOutlet, GravitySliderComponent, X86LoopComponent,
-    TopMenuComponent, CubeComponent, PhoneComponent, LeftRightComponent
+    CubeComponent, PhoneComponent, LeftRightComponent, NgOptimizedImage, ClusterContainerComponent
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
